b7d63da8d036b42b7fa615a8575f60ae53890679
Vincent Attali <vattali@localhost.localdomain>
2017-11-14 15:20:58 +0100

    "fixed to high indentation"

--- lib/src/my_printf.c
+++ lib/src/my_printf.c
@@ -48,26 +48,26 @@ void convert_hexa(int mode, int nb)
 
 int my_printf(char *str, ...)
 {
-	int i;
+	int i = 0;
 	va_list ap;
 
 	va_start(ap, str);
-	for (i = 0; str[i]; i++) {
-		if (str[i] == '%') {
-			switch (str[i + 1]) {
-			case 's' : my_putstr(va_arg(ap, char*)); break;
-			case 'd' :
-			case 'i' : my_put_nbr(va_arg(ap, int)); break;
-			case 'c' : my_putchar(va_arg(ap, int)); break;
-			case '%' : my_putchar('%'); break;
-			case 'o' : convert_octal(va_arg(ap, int)); break;
-			case 'x' : convert_hexa(0, va_arg(ap, int)); break;
-			case 'X' : convert_hexa(1, va_arg(ap, int)); break;
-			case 'u' : my_put_nbr(va_arg(ap, unsigned int); break;
-			}
-			i++;
-		} else
-			my_putchar(str[i]);
+	if (str[i] == '%') {
+		switch (str[i + 1]) {
+		case 's' : my_putstr(va_arg(ap, char*)); break;
+		case 'd' :
+		case 'i' : my_put_nbr(va_arg(ap, int)); break;
+		case 'c' : my_putchar(va_arg(ap, int)); break;
+		case '%' : my_putchar('%'); break;
+		case 'o' : convert_octal(va_arg(ap, int)); break;
+		case 'x' : convert_hexa(0, va_arg(ap, int)); break;
+		case 'X' : convert_hexa(1, va_arg(ap, int)); break;
+		case 'u' : my_put_nbr(va_arg(ap, unsigned int)); break;
+		}
+		i += 2;
+	} else {
+		my_putchar(str[i]);
+		i += 1;
 	}
 	va_end(ap);
 	return (0);
