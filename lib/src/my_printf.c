7830fd43f28bbb113f20a4e3a3e0e8fa034ecdaf
Vincent Attali <vattali@localhost.localdomain>
2017-11-15 11:12:10 +0100

    "re-added for"

--- lib/src/my_printf.c
+++ lib/src/my_printf.c
@@ -15,22 +15,22 @@ int my_printf(char *str, ...)
 	va_list ap;
 
 	va_start(ap, str);
-	if (str[i] == '%') {
-		switch (str[i + 1]) {
-		case 's': my_putstr(va_arg(ap, char*)); break;
-		case 'd':
-		case 'i': my_put_nbr(va_arg(ap, int)); break;
-		case 'c': my_putchar(va_arg(ap, int)); break;
-		case '%': my_putchar('%'); break;
-		case 'o': convert_octal(va_arg(ap, int)); break;
-		case 'x': convert_hexa(0, va_arg(ap, int)); break;
-		case 'X': convert_hexa(1, va_arg(ap, int)); break;
-		case 'u': my_put_nbr(va_arg(ap, unsigned int)); break;
-		}
-		i += 2;
-	} else {
-		my_putchar(str[i]);
-		i += 1;
+	for (i = 0; str[i]; i++) {
+		if (str[i] == '%') {
+			switch (str[i + 1]) {
+			case 's': my_putstr(va_arg(ap, char*)); break;
+			case 'd':
+			case 'i': my_put_nbr(va_arg(ap, int)); break;
+			case 'c': my_putchar(va_arg(ap, int)); break;
+			case '%': my_putchar('%'); break;
+			case 'o': convert_octal(va_arg(ap, int)); break;
+			case 'x': convert_hexa(0, va_arg(ap, int)); break;
+			case 'X': convert_hexa(1, va_arg(ap, int)); break;
+			case 'u': my_put_nbr(va_arg(ap, unsigned int)); break;
+			}
+			i++;
+		} else
+			my_putchar(str[i]);
 	}
 	va_end(ap);
 	return (0);
